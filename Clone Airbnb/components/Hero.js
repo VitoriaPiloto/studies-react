5bc5a755cb0a649617cf0f15fd4e03bcaa3f886d
Vitória Piloto <57605180+VitoriaPiloto@users.noreply.github.com>
2023-03-11 19:02:50 -0300

    aplicação usando conceitos de props e map

---
+++ Clone Airbnb/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/photo-grid.png" className="hero--photo" />
+            <h1 className="hero--header"> Experiência Online </h1>
+            <p className="hero--text">Participe de atividades interativas exclusivas lideradas por
+             anfitriões únicos - tudo sem sair de casa.</p>
+        </section>
+    )
+}
